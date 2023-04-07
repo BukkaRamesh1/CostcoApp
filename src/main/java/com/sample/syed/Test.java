a6b2529c0d45dbb24fd7194f40dffd47522c8c23
syedahmedIT <syedahmedIT2023@gmail.com>
2023-04-07 15:05:46 -0400

    sample code

---
+++ src/main/java/com/sample/syed/Test.java
@@ -0,0 +1,31 @@
+package com.sample.syed;
+
+
+
+/*
+ * 
+ *   
+ *   
+ *   
+ *    
+ * 
+ * 
+ * 
+ */
+
+
+/*
+*public class Test {
+*	
+*	public static void main(String[] args) {
+*	-9622	System.out.println("hello");
+*	}
+*
+*}
+*/
+public class Test {
+	public static void main (String[] args) {
+		System.out.println("test");
+		
+	}
+}
