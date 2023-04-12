5696299c194dec84a06d84e4b603da480e6cccba
amega <amega@LAPTOP-7H2917PC.mynetworksettings.com>
2023-04-12 07:45:02 -0400

    abdel

---
+++ src/main/java/com/sample/Customer.java
@@ -0,0 +1,16 @@
+package com.sample;
+
+public class Customer {
+ 
+ 	
+ 	
+ 	public static void main (String[] args) {
+		
+		System.out.println("Hello");
+		
+ 	}	
+}
+
+// create user save method
+
+
